41a936bce623f2d35216c7f14e9491bab38ddd1b
Dmitry Pushkin <beliar1232@gmail.com>
2026-05-01 14:47:55 +0300

    static files

--- reminder/api/app.py
+++ reminder/api/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
 
 from reminder.api.middleware.logging import RequestLoggingMiddleware
 from reminder.api.routers import auth, events, history, users
@@ -24,6 +25,7 @@ app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(events.router)
 app.include_router(history.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.exception_handler(Exception)
@@ -32,3 +34,8 @@ async def unhandled_exception_handler(request: Request, exc: Exception) -> JSONR
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         content={"detail": "Internal server error"},
     )
+
+
+@app.get("/")
+async def index_page():
+    return FileResponse("static/Reminder.html")
